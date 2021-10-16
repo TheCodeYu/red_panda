b3289521587d97029cd16a9473300908b59a2343
zhouyu <zhouyu@techvision.com.cn>
2021-10-16 11:30:25 +0800

    add example for vue3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/display.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
 import router from './router'
